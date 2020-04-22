09261f208e63d7a19bdb806af672b907353a4518
LujainD <dweikatl@tcd.ie>
2020-04-22 14:10:58 +0100

    fetch garda, healthcare, and firebrigade.

--- MinuteDublin/emergency_center/urls.py
+++ MinuteDublin/emergency_center/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('create_all', views.create_all, name='create_all'),
     path('fetch_fire_brigade', views.fetch_fire_brigade, name='fetch_fire_brigade'),
     path('fetch_garda', views.fetch_garda, name='fetch_garda'),
-    path('fetch_healthcare', views.fetch_garda, name='fetch_healthcare')
+    path('fetch_healthcare', views.fetch_healthcare, name='fetch_healthcare')
 
 ]
