bd40b7f02e208b2c7794c0c93f4c77ef52d84e4b
LujainD <dweikatl@tcd.ie>
2020-04-22 14:05:58 +0100

    fetch garda, healthcare, and firebrigade.

--- MinuteDublin/emergency_center/urls.py
+++ MinuteDublin/emergency_center/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('create_all', views.create_all, name='create_all'),
     path('fetch_fire_brigade', views.fetch_fire_brigade, name='fetch_fire_brigade'),
+    path('fetch_garda', views.fetch_garda, name='fetch_garda'),
+    path('fetch_healthcare', views.fetch_garda, name='fetch_healthcare')
 
 ]
--- MinuteDublin/emergency_center/views.py
+++ MinuteDublin/emergency_center/views.py
@@ -5,6 +5,9 @@ import fire_stations.firestations_url2lib as fire
 import garda_stations.garda_url2lib as garda
 import hospitals.hospitals_url2lib as hospital
 from django.http import HttpResponse
+from django.http import JsonResponse
+import dynamodb.dynamodb_access as ddba
+
 
 
 def create_all(request):
@@ -14,5 +17,75 @@ def create_all(request):
     return HttpResponse(status=200)
 
 
-def fetch_fire_brigade():
-    return
+def fetch_fire_brigade(request):
+
+    stations = ddba.get_all_firestations()
+
+    geo_json = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for station in stations:
+        item = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [ float(station["longitude"]), float(station["latitude"])]
+          },
+          "properties": {
+            "name": station["name"]
+          }
+        }
+        geo_json["features"].append(item)
+
+    return JsonResponse(geo_json, safe=False, json_dumps_params={"indent": 2})
+
+
+def fetch_healthcare(request):
+
+    stations = ddba.get_all_hospitals()
+
+    geo_json = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for station in stations:
+        item = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [ float(station["longitude"]), float(station["latitude"])]
+          },
+          "properties": {
+            "name": station["name"]
+          }
+        }
+        geo_json["features"].append(item)
+
+    return JsonResponse(geo_json, safe=False, json_dumps_params={"indent": 2})
+
+def fetch_garda(request):
+
+    stations = ddba.get_all_gardastations()
+
+    geo_json = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for station in stations:
+        item = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [ float(station["longitude"]), float(station["latitude"])]
+          },
+          "properties": {
+            "name": station["name"]
+          }
+        }
+        geo_json["features"].append(item)
+
+    return JsonResponse(geo_json, safe=False, json_dumps_params={"indent": 2})
