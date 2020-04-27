c26197a88f8551f50f37e0f839fe83df88ca3460
LujainD <dweikatl@tcd.ie>
2020-04-27 11:27:30 +0100

    changes in heirarchy

--- MinuteDublin/stop/urls.py
+++ MinuteDublin/stop/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('train', views.train, name='train'),
     path('train_geo_json', views.train_geo_json, name='train_geo_json'),
+    path('bus_stops_geo_json', views.bus_stops_geo_json, name='bus_stops_geo_json'),
 
 ]
--- MinuteDublin/stop/views.py
+++ MinuteDublin/stop/views.py
@@ -65,3 +65,27 @@ def train_geo_json(request):
     return JsonResponse(geo_json, safe=False, json_dumps_params={"indent": 2})
 
 
+def bus_stops_geo_json(request):
+    bus_stop_data = ddba.get_all_bus_stops()
+
+    geo_json = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for bus_stop in bus_stop_data:
+        stop = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [float(bus_stop["longitude"]), float(bus_stop["latitude"])]
+          },
+          "properties": {
+            "name": bus_stop["name"],
+            "id": bus_stop["id"],
+#             "timestamp": bus_stop["time_fetched"],
+            "type":"train",
+          }
+        }
+        geo_json["features"].append(stop)
+    # Getting JSON response
+    return JsonResponse(geo_json, safe=False, json_dumps_params={"indent": 2})
